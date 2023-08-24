8cb52475cc6410f5e7ebbe19a4951c30bc61ef2f
Becky Hu <bh456@cornell.edu>
2023-08-24 18:55:54 -0400

    jeri template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { ImageViewer } from 'jeri';
 import './App.css';
 
-function App() {
+const configForImage = (imageName, spp, isHomogeneous) => {
+  const gs = [0, 0.5, 0.9];
+  return isHomogeneous
+    ? {
+        title: 'root',
+        children: gs.map((g) => {
+          return {
+            title: `g=${g}`,
+            children: [
+              {
+                title: 'Image',
+                image: `${process.env.PUBLIC_URL}/img/${imageName}/PGv3_g=${g}_${spp}spp.exr`,
+              },
+              {
+                title: 'Reference',
+                image: `${process.env.PUBLIC_URL}/img/${imageName}/ref_g=${g}.exr`,
+              },
+            ],
+          };
+        }),
+      }
+    : {
+        title: 'root',
+        children: [
+          {
+            title: 'Image',
+            image: `${process.env.PUBLIC_URL}/img/${imageName}/PGv3_${spp}spp.exr`,
+          },
+          {
+            title: 'Reference',
+            image: `${process.env.PUBLIC_URL}/img/${imageName}/ref.exr`,
+          },
+        ],
+      };
+};
+
+const App = () => {
+  // conditional rendering of the spps
+  // const [spp, setSpp] = useState(10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div style={{ position: 'relative', height: '300px' }}>
+        <ImageViewer data={configForImage('sphere', 10, true)} />
+        {/* <img src={process.env.PUBLIC_URL + '/img/ra.png'} alt='ra' /> */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
